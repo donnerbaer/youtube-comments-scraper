df673f0da0151209aeaa26e2187526b7ccb6a1ee
donnerbaer <45535297+donnerbaer@users.noreply.github.com>
2024-03-31 18:51:33 +0200

    Update version.py

--- version.py
+++ version.py
@@ -5,15 +5,23 @@ from googleapiclient import version as googleapiclient_version
 
 
 class Version:
-    """_summary_
+    """A class that represents the version information.
+
+    This class provides methods to print the required version and installed version
+    of the needed libraries.
+
+    Attributes:
+        None
+
+    Methods:
+        __init__(): Initializes the Version object and prints the version information.
     """
 
     def __init__(self) -> None:
-        """ print version required version and installed version of needed libraries
-        """
+        """Initializes the Version object and prints the version information."""
         print()
-        print(f'library          | required | installed ')
-        print(f'---------------- | -------- | ----------------------------')
+        print( 'library          | required | installed ')
+        print( '---------------- | -------- | ----------------------------')
         print(f'python           | 3.12.2   | {sys.version}')
         print(f'googleapiclient  | 2.121.0  | {googleapiclient_version.__version__}')
         print(f'sqlite3          | 3.43.1   | {sqlite3.sqlite_version}')
